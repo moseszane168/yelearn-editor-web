ef01920460dc5f7275ea3b952d2690a491b1e9f9
sanxi.zhang <sanxi.zhang@dev.com>
2024-09-14 14:02:26 +0800

    句子页面联调

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from './router/index'
 import store from './store'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
@@ -15,15 +15,15 @@ import zhLocale from 'element-ui/lib/locale/lang/zh-CN'
 
 
 // 单独引入样式文件
-import 'element-ui/lib/theme-chalk/index.css';
+//import 'element-ui/lib/theme-chalk/index.css';
 // 加载拖拽组件
-import './utils/directives'
+//import './utils/directives'
 // import env from './env'
 
 
 // 执行Element UI
 Vue.use(ElementUI, {
-  size: 'small',
+  //size: 'small',
 });
 
 import { Pagination, Table,Select } from 'element-ui';
@@ -31,50 +31,51 @@ Vue.use(Pagination);
 Vue.use(Table)
 Vue.use(Select)
 // 引入Element UI时，可以通过传入一个配置对象，来对Element UI进行配置
-import VueI18n from 'vue-i18n';
+//import VueI18n from 'vue-i18n';
 // 引入国际化插件设定的不同语言的文本
-import { messages } from './utils/i18n';
-import eleLocale from 'element-ui/lib/locale';
+//import { messages } from './utils/i18n';
+//import eleLocale from 'element-ui/lib/locale';
 
 // 引入国际化插件
-Vue.use(VueI18n);
+//Vue.use(VueI18n);
 
 // 实例化国际化插件,用于实现多语言功能
-const i18n = new VueI18n({
+/*const i18n = new VueI18n({
   locale: 'zh',
   // 指定当前使用的语言
   messages,
   // 包含了不同语言文本的对象
-  
-});
-Vue.use(ElementUI, {
-  i18n: (key, value) => i18n.t(key, value)
-})
-eleLocale.i18n((key, value) => i18n.t(key, value))
+});*/
 
-const UIlocales = {
+/*Vue.use(ElementUI, {
+  i18n: (key, value) => i18n.t(key, value)
+})*/
+
+//eleLocale.i18n((key, value) => i18n.t(key, value))
+
+/*const UIlocales = {
   zh: zhLocale,
   en: enLocale
-}
+}*/
 
 // 通过判断lang语言标志符来return先对应的语言
-const setUIlocales = lang =>{
+/*const setUIlocales = lang =>{
   switch (lang) {
     case 'zh': return UIlocales.zh
     case 'en': return UIlocales.en
   }
-}
+}*/
 
-eleLocale.use(enLocale) // element ui 切换语言
+//eleLocale.use(enLocale) // element ui 切换语言
 
 Vue.config.productionTip = false
 //加载图标样式
-import './assets/css/icon.css';
-import './assets/css/Icon-css/iconfont.css'
+//import './assets/css/icon.css';
+//import './assets/css/Icon-css/iconfont.css'
 //加载拖拽组件
-import './utils/directives';
+//import './utils/directives';
 //加载语法编辑器
-import 'babel-polyfill';
+//import 'babel-polyfill';
 // 路由的导航守卫，它的作用便是通过跳转或取消的方式守卫导航。我们可以通过router.beforeEach来注册一个全局前置守卫，对路由的跳转进行一些设置
 
 
@@ -88,55 +89,48 @@ import 'babel-polyfill';
 
 axios.defaults.timeout = 10000;
 // 本地环境
- axios.defaults.basePicURL = 'http://127.0.0.1:81/';
+axios.defaults.basePicURL = 'http://127.0.0.1:8081/';
 
-axios.defaults.baseURL = axios.defaults.basePicURL + 'index.php';
+axios.defaults.baseURL = axios.defaults.basePicURL + 'v1';
 
 // 根据环境变量获取不同的请求地址
 // axios.defaults.baseURL = env.baseURL;
 
-
-
-
 // 接口错误拦截
-axios.interceptors.response.use(
+/*axios.interceptors.response.use(
+    function (response) {
+                let res = response.data;
 
+                if (res.code == 200) {
+                  return res.data;
+                } else if (res.code == 403) {
+                  next('/403');
+                } else if (res.code == 401) {
+                  window.location.href = '/index';
+                  return Promise.reject(res);
+                } else if (res.code == 201) {
+                  // window.location.href = '/#/login';
+                  // this.$message.error(res);
 
-  function (response) {
-    let res = response.data;
+                  // Message.error(res.msg);
+                  Message.error({message:res.msg,type: 'error',offset: window.screen.height / 3,customClass: "messageIndex"});
+                  // Message.error({message:res.msg,type: 'error', duration: 5 * 1000,offset:80});
+                  this.$message.error(res);
+                  return Promise.reject(res);
+                } else {
+                  Message.warning(res.msg);
+                  return Promise.reject(res);
+                }
+    },
 
-
-    if (res.code == 200) {
-
-      return res.data;
+    (error) => {
+                let res = error.response;
+                console.log(error);
+                Message.error(res.msg);
+                // Message.error(res.data.message);
+                return Promise.reject(error);
     }
-    else if (res.code == 403) {
-      next('/403');
-    }
-
-    else if (res.code == 401) {
-      window.location.href = '/#/login';
-      return Promise.reject(res);
-    } else if (res.code == 201) {
-      // window.location.href = '/#/login';
-      // this.$message.error(res);
-
-      // Message.error(res.msg);
-      Message.error({message:res.msg,type: 'error',offset: window.screen.height / 3,customClass: "messageIndex"});
-      // Message.error({message:res.msg,type: 'error', duration: 5 * 1000,offset:80});
-      this.$message.error(res);
-      return Promise.reject(res);
-    } else {
-      Message.warning(res.msg);
-      return Promise.reject(res);
-    }
-  }, (error) => {
-    let res = error.response;
-    console.log(error);
-    Message.error(res.msg);
-    // Message.error(res.data.message);
-    return Promise.reject(error);
-  });
+);*/
 
 
 
@@ -145,9 +139,12 @@ axios.interceptors.response.use(
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
   document.title = `${to.meta.title} | vue-manage-system`;
-  const role = localStorage.getItem('ms_username');
-  if (!role && to.path !== '/login') {
-    next('/login');
+
+  //const role = localStorage.getItem('ms_username');
+  const role = "111"
+
+  if (!role && to.path !== '/index') {
+    next('/index');
   } else if (to.meta.permission) {
     // 如果是管理员权限则可进入，这里只是简单的模拟管理员权限而已
     role === 'AsunaCC' ? next() : next('/403');
@@ -165,14 +162,12 @@ router.beforeEach((to, from, next) => {
 
 Vue.use(VueAxios, axios);
 Vue.use(VueCookie);
-Vue.use(VueLazyLoad, {
-  loading: '/imgs/loading-svg/loading-bars.svg'
-})
+Vue.use(VueLazyLoad, {loading: '/imgs/loading-svg/loading-bars.svg'})
 Vue.prototype.$message = Message;
 Vue.config.productionTip = false;
 
 // 在Vue初始化之前添加原型方法
-Vue.prototype.FormatTime = function (time) {
+/*Vue.prototype.FormatTime = function (time) {
   // console.log('Hello, Vue!');
   // console.log(time);
   const dateObj = new Date(time);
@@ -186,7 +181,7 @@ Vue.prototype.FormatTime = function (time) {
   // console.log(time);
 
   return time;
-}
+}*/
 
 // 在Vue初始化之后添加原型方法
 // var vm = new Vue({
@@ -200,7 +195,7 @@ Vue.prototype.FormatTime = function (time) {
 
 new Vue({
   // 挂载插件
-  i18n,
+  //i18n,
   router,
   store,
   render: h => h(App),
